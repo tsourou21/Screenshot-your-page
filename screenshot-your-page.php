b20c960ba609b48babfc61a22652099fb6dffe5f
Anastasios Tsourounis <tsourousparti@outlook.com>
2022-03-24 14:11:02 +0200

    Screenshot file

---
+++ screenshot-your-page.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Screenshot your page
+ * Plugin URI: https://www.anastasys.gr
+ * Description: Display FAQ content and Script shema using a shortcode to insert in a page or post [anastasys-schema question=" | " answer=" | "]
+ * Version: 0.1
+ * Text Domain: anastasys.gr
+ * Author: Anastasios Tsourounis
+ * Author URI: https://www.anastasys.gr
+ */
+//Receive an array of texts and display it
