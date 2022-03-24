5b8aa17a90a7794dd1dc4f64116ae3f1a8c40bb1
Dimitris Margelos <jimmarg321@gmail.com>
2022-03-24 14:59:39 +0200

    I change the description

--- screenshot-your-page.php
+++ screenshot-your-page.php
@@ -2,10 +2,9 @@
 /**
  * Plugin Name: Screenshot your page
  * Plugin URI: https://www.anastasys.gr
- * Description: Display FAQ content and Script shema using a shortcode to insert in a page or post [anastasys-schema question=" | " answer=" | "]
+ * Description: Plugin to take a screenshot of your current page and download it to your device
  * Version: 0.1
  * Text Domain: anastasys.gr
- * Author: Anastasios Tsourounis
+ * Authors: Anastasios Tsourounis, Dimitris Margelos, Andreas Andrikopoulos
  * Author URI: https://www.anastasys.gr
  */
-//Receive an array of texts and display it
