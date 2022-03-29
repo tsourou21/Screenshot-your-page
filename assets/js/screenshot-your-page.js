8a9bed6cf3e406370929fa87a9819fef6c80db77
Anastasios Tsourounis <tsourousparti@outlook.com>
2022-03-29 16:32:09 +0300

    if adminbar

--- assets/js/screenshot-your-page.js
+++ assets/js/screenshot-your-page.js
@@ -2,19 +2,36 @@ let btns = document.getElementsByClassName('scrshot-btn');
 
 for (let i=0; i < btns.length; i++) {
     btns[i].addEventListener('click', function() {
-        document.getElementById('wpadminbar').style.display = 'none';
-        let element = document.getElementsByTagName('body')[0];
-            const options = {
-                letterRendering: true
-            };
-    
-            html2canvas(element, options).then(function(canvas) {      
-    
-            const link = document.createElement('a');
-            link.download = 'scan-and-save.png';
-            link.href = canvas.toDataURL();
-            link.click();
-        });
-        document.getElementById('wpadminbar').style.display = 'block';
+        let adminBar = document.getElementById('wpadminbar');
+        if(adminBar){
+            document.getElementById('wpadminbar').style.display = 'none';
+            let element = document.getElementsByTagName('body')[0];
+                const options = {
+                    letterRendering: true
+                };
+        
+                html2canvas(element, options).then(function(canvas) {      
+        
+                const link = document.createElement('a');
+                link.download = 'scan-and-save.png';
+                link.href = canvas.toDataURL();
+                link.click();
+            });
+            document.getElementById('wpadminbar').style.display = 'block';  
+        }else{
+            let element = document.getElementsByTagName('body')[0];
+                const options = {
+                    letterRendering: true
+                };
+        
+                html2canvas(element, options).then(function(canvas) {      
+        
+                const link = document.createElement('a');
+                link.download = 'scan-and-save.png';
+                link.href = canvas.toDataURL();
+                link.click();
+            });
+        }
+        
     }); 
 }
